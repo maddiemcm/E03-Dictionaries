ee24823e6a71d2f8fe315e1abd2f1e157df70f43
McMullen <mabmcmul@iu.edu>
2019-09-11 19:12:15 -0400

    Started Dictionaries Assignment

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Welcome to the Birthday Catalog of Game Changers')
+
+birthday_dictionary = {
+    "Michelangelo": "March 6, 1475",
+    "Leonardo da Vinci": "April 15, 1452",
+    "Marie Curie": "November 7, 1867"
+}
+
+print(birthday_dictionary[Michelangelo])
