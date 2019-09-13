bcbe835e3dcd600cacb39fbcea0ea085289bcc39
maddiemcm <54558273+maddiemcm@users.noreply.github.com>
2019-09-12 22:57:00 -0400

    updating? sorta????
    HELP

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-print('Welcome to the Birthday Catalog of Game Changers')
+if __name__ == '__main__':
+    
+    birthdays = {
+        'Michelangelo': 'March 6, 1475',
+        'Leonardo da Vinci': 'April 15, 1452',
+        'Marie Curie': 'November 7, 1867'}
 
-birthday_dictionary = {
-    "Michelangelo": "March 6, 1475",
-    "Leonardo da Vinci": "April 15, 1452",
-    "Marie Curie": "November 7, 1867"
-}
 
-print(birthday_dictionary[Michelangelo])
+    print('Welcome to the Birthday Dictionary. We know the birthdays of:')
+    for name in birthdays:
+        print(name)
+
+    print('Who\'s birthday do you want to look up?')
+    name = input()
+    if name in birthdays:
+        print('{}\'s birthday was born on {}.'.format(name, birthdays[name]))
+    else:
+        print('Please use a name from the provided list.')
+
+    # Can i put "print(birthdays.get(name, 'Please use a name from the provided list.'))
+
+   ###### ATTEMPT 2 
+    
+    if __name__ == '__main__':
+    
+    birthdays = {
+        'Michelangelo': 'March 6, 1475',
+        'Leonardo da Vinci': 'April 15, 1452',
+        'Marie Curie': 'November 7, 1867'}
+
+    print('Welcome to the Birthday Dictionary. We know the birthdays of:')
+    for name in birthdays:
+        print(name)      
+        
+    print('Would you like to access the birthday dictionary?')
+    'response' = input()
+    While 'response' != 'no'): 
+        print('Who\'s birthday do you want to look up?')
+        name = input()
+        for name in birthdays:
+            print(birthdays.get(name, 'Please use a name from the provided list.'))
+    else:      
+        print('Thank you for using the birthday dictionary!")
+        
+    
+
+    
+
+    # Can i put "print(birthdays.get(name, 'Please use a name from the provided list.'))
+    # print('{}\'s birthday was born on {}.'.format(name, birthdays[name]))
+        # else:
+            #print('Please use a name from the provided list.')
