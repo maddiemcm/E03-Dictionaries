e580e2e7aca6e4681659f73fc2667b46100ea169
McMullen <mabmcmul@iu.edu>
2019-09-16 18:41:12 -0400

    Updated Assignment

--- main.py
+++ main.py
@@ -10,16 +10,20 @@ if __name__ == '__main__':
         'John Powell':'September 18, 1963',
         'Josh Gad':'February 23, 1981'}
 
+    play = "yes"
+    while play == "yes":
+        print('Welcome to the Birthday Dictionary. We know the birthdays of:')
+        for name in birthdays:
+            print(name)
 
-    print('Welcome to the Birthday Dictionary. We know the birthdays of:')
-    for name in birthdays:
-        print(name)
+        print('\n\nWho\'s birthday do you want to look up?')
+        name = input()
+        if name in birthdays:
+            print('{} was born on {}.'.format(name, birthdays[name]))
+        else:
+            print('Please use a name from the provided list.')
+        play = input("Do you want to play the game again? ").lower().strip()
 
-    print('\n\nWho\'s birthday do you want to look up?')
-    name = input()
-    if name in birthdays:
-        print('{} was born on {}.'.format(name, birthdays[name]))
-    else:
-        print('Please use a name from the provided list.')
+
 
 
